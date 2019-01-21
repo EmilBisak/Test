d9f258ba79faf7e09ad8bb8af20dfaa755c5b2fa
EmilBisak <bisak.emil@gmail.com>
2019-01-21 20:20:47 +0100

    add bootstrap and css

--- Task2_Logic/main.js
+++ Task2_Logic/main.js
@@ -1,10 +1,41 @@
 let lettersElement = document.querySelector(".letters");
 
-let lettersArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let lettersArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-for (let i = 0; i < lettersArr.length; i++) {
-    const element = lettersArr[i];
-    lettersElement.innerHTML += `<div class="letter-wrapper"><h2>${element} (${i+1})</h2></div>`
-    
-    
+createLetterList();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createLetterList() {
+    for (let i = 0; i < lettersArr.length; i++) {
+        const element = lettersArr[i];
+        lettersElement.innerHTML += `<div class="letter-wrapper"><h2>${element} (${i + 1})</h2></div>`
+    }
 }
+
