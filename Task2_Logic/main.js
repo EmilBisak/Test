053b91e513458373a9325a98e6cbb17526b64669
EmilBisak <bisak.emil@gmail.com>
2019-01-23 02:28:40 +0100

    make option to change game difficulty

--- Task2_Logic/main.js
+++ Task2_Logic/main.js
@@ -5,11 +5,14 @@ let hitElement = document.querySelector(".hit");
 let missElement = document.querySelector(".miss");
 let leftElement = document.querySelector(".left");
 let startBtn = document.querySelector("#start-btn");
+let easyBtn = document.querySelector("#easy");
+let mediumBtn = document.querySelector("#medium");
+let hardBtn = document.querySelector("#hard");
 
 let lettersArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numberArr = [];
 let pastNumberArr = [];
-let level = 500;
+let level = 350;
 let count = level;
 let numberOfPassedLetters = 0;
 let lettersLeft = 26;
@@ -26,19 +29,62 @@ window.onload = init();
 
 
 letterInput.addEventListener("keyup", matchTypedLetterWithCurrentLetter);
-startBtn.addEventListener("click", startStopGame)
+startBtn.addEventListener("click", startStopGame);
+easyBtn.addEventListener("click", changeLevel);
+mediumBtn.addEventListener("click", changeLevel);
+hardBtn.addEventListener("click", changeLevel);
 
 
 function startStopGame() {
     isPlaying = !isPlaying;
+    count = level;
+
     if (isPlaying) {
-        letterInput.focus()
+
+        letterInput.focus();
         pastNumberArr.push(numberArr[numberOfPassedLetters])
         counter = setInterval(timer, 10);
         startBtn.innerHTML = "Stop Game";
+        startBtn.className = "btn btn-danger";
+        easyBtn.removeEventListener("click", changeLevel);
+        mediumBtn.removeEventListener("click", changeLevel);
+        hardBtn.removeEventListener("click", changeLevel);
+        easyBtn.children[0].setAttribute("disabled", true);
+        mediumBtn.children[0].setAttribute("disabled", true);
+        hardBtn.children[0].setAttribute("disabled", true);
+
     } else {
+
         clearInterval(counter);
-        startBtn.innerHTML = "Start Game"
+        startBtn.innerHTML = "Start Game";
+        startBtn.className = "btn btn-primary";
+
+    }
+}
+
+
+function changeLevel() {
+
+    switch (this.id) {
+
+        case "easy":
+            this.children[0].checked = true;
+            level = 500;
+            break;
+
+        case "medium":
+            this.children[0].checked = true;
+            level = 350;
+            break;
+
+        case "hard":
+            this.children[0].checked = true;
+            level = 200;
+            break;
+
+        default:
+            level = 350;
+            break;
     }
 }
 
@@ -46,24 +92,19 @@ function startStopGame() {
 function matchTypedLetterWithCurrentLetter(e) {
     inputLetterTyped = e.key.toUpperCase();
     isTyped = true;
+    current = lettersArr[numberArr[numberOfPassedLetters] - 1];
+
     if (isTyped) {
         letterInput.removeEventListener("keyup", matchTypedLetterWithCurrentLetter);
     }
-    console.log("inputLetterTyped - ", inputLetterTyped);
-    current = lettersArr[numberArr[numberOfPassedLetters] - 1]
-    if (inputLetterTyped === current) {
-        console.log("CORRECT!!!");
-        hitLetterArr.push(current)
-        console.log("hitLetterArr -- ", hitLetterArr);
 
+    if (inputLetterTyped === current) {
+        hitLetterArr.push(current)
         hitElement.innerHTML = `Hit: ${hitLetterArr.length}`;
         pastNumberArr.pop()
-    } else {
-        console.log("WRONG!!!");
-
     }
-    createLetterList()
 
+    createLetterList()
 }
 
 
@@ -71,14 +112,14 @@ function timer() {
 
     if (count >= 0 && numberOfPassedLetters < 26) {
 
-        // currentLetter.innerHTML = count / 100 + " secs";
         currentLetter.innerHTML = numberArr[numberOfPassedLetters];
 
     } else if (numberOfPassedLetters <= 26) {
+
+        count = level;
         letterInput.value = "";
         createLetterList()
         letterInput.addEventListener("keyup", matchTypedLetterWithCurrentLetter);
-        count = level;
         numberOfPassedLetters++;
         lettersLeft--;
         missElement.innerHTML = `Miss: ${pastNumberArr.length}`;
@@ -88,18 +129,13 @@ function timer() {
         pastLetters.push(numberArr[numberOfPassedLetters]);
 
     } else {
-        clearInterval(counter);
-        startBtn.innerHTML = "Restart Game";
-        startBtn.addEventListener("click", () => { window.location.reload() });
-        startBtn.focus();
-        currentLetter.innerHTML = "Game Over";
-        currentLetter.style.color = "#788896";
+        newGame()
         return;
 
     }
+
     count--;
 }
-
 
 
 function shuffle(array) {
@@ -114,7 +150,6 @@ function shuffle(array) {
 }
 
 
-
 function createLetterList() {
     lettersElement.innerHTML = "";
     for (let i = 0; i < lettersArr.length; i++) {
@@ -126,12 +161,12 @@ function createLetterList() {
     LetterIsMissed();
 }
 
+
 function createNumberArr() {
     for (let i = 0; i < lettersArr.length; i++) {
         numberArr.push(i + 1);
     }
 }
-
 
 
 function LetterIsMissed() {
@@ -143,6 +178,16 @@ function LetterIsMissed() {
     });
 }
 
+
+function newGame() {
+    clearInterval(counter);
+    startBtn.innerHTML = "Restart Game";
+    startBtn.className = "btn btn-warning";
+    startBtn.addEventListener("click", () => { window.location.reload() });
+    startBtn.focus();
+    currentLetter.innerHTML = "Game Over";
+    currentLetter.style.color = "#788896";
+}
 
 
 function init() {
